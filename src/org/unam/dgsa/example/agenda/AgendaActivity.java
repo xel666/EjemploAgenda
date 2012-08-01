acf0528170b3ee47a661f713bfdb29b5e186f60f
xel666 <xel666@comunidad.unam.mx>
2012-08-01 11:23:29 -0500

    Contact adapter implemented.

--- src/org/unam/dgsa/example/agenda/AgendaActivity.java
+++ src/org/unam/dgsa/example/agenda/AgendaActivity.java
@@ -1,15 +1,43 @@
 package org.unam.dgsa.example.agenda;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import org.unam.dgsa.example.agenda.db.Contacto;
+import org.unam.dgsa.example.agenda.db.Telefono;
 
-public class AgendaActivity extends Activity {
+import java.util.ArrayList;
+
+public class AgendaActivity extends ListActivity {
+    public static final ArrayList<Contacto> contactos=
+            new ArrayList<Contacto>();
+
+    static {
+        Contacto c = new Contacto("UNAM", "DGSA");
+        c.getTelefonos().add(new Telefono(c, "65496849684"));
+        contactos.add(c);
+    }
+
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setListAdapter(new ContactAdapter(this, contactos));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()!=R.id.mni_nuevo) return false;
+        return true;
     }
 }
---
+++ src/org/unam/dgsa/example/agenda/ContactAdapter.java
@@ -0,0 +1,59 @@
+package org.unam.dgsa.example.agenda;
+
+import android.*;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import org.unam.dgsa.example.agenda.db.Contacto;
+import org.unam.dgsa.example.agenda.db.Telefono;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xel666
+ * Date: 1/08/12
+ * Time: 9:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContactAdapter extends ArrayAdapter<Contacto> {
+    private LayoutInflater mInflater;
+
+    public ContactAdapter(Context context, List<Contacto> data) {
+        super(context, 0, 0, data);
+        mInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ValueTag tag;
+        if(convertView==null) {
+            convertView=mInflater.inflate(android.R.layout.simple_list_item_2,
+                    parent, false);
+            tag = new ValueTag();
+            tag.nombre = (TextView) convertView.findViewById(android.R.id.text1);
+            tag.telefono = (TextView) convertView.findViewById(android.R.id.text2);
+            convertView.setTag(tag);
+        } else {
+            tag= (ValueTag) convertView.getTag();
+        }
+        Contacto c = getItem(position);
+        tag.nombre.setText(c.getNombre()+' '+c.getApellido());
+        if(c.getTelefonos().isEmpty())
+            tag.telefono.setText(R.string.msg_tels_empty);
+        else {
+            Telefono t = c.getTelefonos().iterator().next();
+            tag.telefono.setText(t.getValue());
+        }
+        return convertView;
+    }
+
+    private class ValueTag {
+        TextView nombre;
+        TextView telefono;
+    }
+}
